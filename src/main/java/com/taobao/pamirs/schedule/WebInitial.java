e39c6b3f83d64ce64b366b2bfb781d8f0b41203e
cjq <ppqq1121827@163.com>
2018-12-28 10:41:47 +0800

    修改打包方式

--- src/main/java/com/taobao/pamirs/schedule/WebInitial.java
+++ src/main/java/com/taobao/pamirs/schedule/WebInitial.java
@@ -3,9 +3,13 @@ package com.taobao.pamirs.schedule;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class WebInitial extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	protected static transient Logger log = LoggerFactory.getLogger(WebInitial.class);
 
 	public void init() throws ServletException {
 		super.init();
